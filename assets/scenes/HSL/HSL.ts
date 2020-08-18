990d080c666c6da86ed8bd2ff2a7700f9e365d11
wangdonglin <50353438@qq.com>
2020-08-18 12:27:15 +0800

    init

---
+++ assets/scenes/HSL/HSL.ts
@@ -0,0 +1,64 @@
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class HSL extends cc.Component {
+
+    @property(cc.Sprite)
+    spCocos: cc.Sprite = null;
+    @property(cc.Label)
+    numLabelH: cc.Label = null;
+    @property(cc.Label)
+    numLabelS: cc.Label = null;
+    @property(cc.Label)
+    numLabelL: cc.Label = null;
+
+    numH: number = 0; // 色相(0, 360)
+    numS: number = 0; // 饱和度(-1, 1)
+    numL: number = 0.5;// 亮度(-1, 1)
+    material:cc.Material = null;
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+    }
+
+    start () {
+        this.material = this.spCocos.getMaterial(0);
+        // this.setHue(this.numH);
+        // this.setSaturation(this.numS);
+        // this.setLightness(this.numL);
+    }
+
+    onSliderHEvent(slider: cc.Slider) {
+        this.setHue(Number(slider.progress * 360));
+    }
+
+    onSliderSEvent(slider: cc.Slider) {
+        this.setSaturation(Number(slider.progress)*2-1);
+    }
+
+    onSliderLEvent(slider: cc.Slider) {
+        this.setLightness(Number(slider.progress)*2-1);
+    }
+
+    setHue(value: number) {
+        this.numLabelH.string = this._formatNum(value).toString();
+        this.material.setProperty('u_dH', value);
+    }
+
+    setSaturation(value: number) {
+        this.numLabelS.string = this._formatNum(value).toString();
+        this.material.setProperty('u_dS', value);
+    }
+
+    setLightness(value: number) {
+        this.numLabelL.string = this._formatNum(value).toString();
+        this.material.setProperty('u_dL', value);
+    }
+
+    private _formatNum(value: number): number {
+        let num = Math.round(value * 100) / 100;
+        return num;
+    }
+}
---
+++ assets/scenes/Main/NameHelper.ts
@@ -0,0 +1,35 @@
+interface nameDesc {
+    name: string;
+    desc: string;
+};
+
+let nameArray: nameDesc[] = [
+    { name:'HSL', desc:'RGB转HSL' }, // 0
+];
+
+export default class NameHelper {
+    static version: string = 'Test Shaders For CocosCreator 2.3.4';
+    
+    static getTitle(index: number): string {
+        let str = '';
+        if (index >= 0 && index < nameArray.length) {
+            str = index + '.    ' + nameArray[index].name + ' - ' + nameArray[index].desc;
+        } else {
+            str = NameHelper.version;
+        }
+        return str;
+    }
+
+    static getName(index: number): string {
+        let name = '';
+        if (index >= 0 && index < nameArray.length) {
+            name = nameArray[index].name;
+        }
+        return name;
+    }
+
+    static getCount(): number {
+        return nameArray.length;
+    }
+
+}
